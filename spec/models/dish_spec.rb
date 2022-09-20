81663996b274fc1bf5a8c49d4a68c1f17dda964d
Ken Lenhart <omnipaid@gmail.com>
2022-09-19 21:28:51 -0700

    test: Add total_calories model test to dish.rb

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe Dish, type: :model do
     it { should have_many(:dish_ingredients) }
     it { should have_many(:ingredients).through(:dish_ingredients) }
   end
+
+  describe '#instance methods' do
+    before :each do
+      @chef_1 = Chef.create!(name: "Chef Boyardee")
+
+      @dish_1 = @chef_1.dishes.create!(name: "Raviolis", description: "Cheap pasta in ketchup sauce")
+      @dish_2 = @chef_1.dishes.create!(name: "Lasagna", description: "Cardboard noodles with sawdust sprinkles")
+
+      @ingredient_1 = @dish_1.ingredients.create!(name: "Ketchup Sauce", calories: 1000)
+      @ingredient_2 = @dish_1.ingredients.create!(name: "Cheap Pasta", calories: 2000)
+
+      @ingredient_3 = @dish_2.ingredients.create!(name: "Cardboard Noodles", calories: 500)
+      @ingredient_4 = @dish_2.ingredients.create!(name: "Sawdust Sprinkles", calories: 1000)
+    end
+
+    it '#total_calories' do
+      expect(@dish_1.total_calories).to eq(3000)
+    end
+  end
 end

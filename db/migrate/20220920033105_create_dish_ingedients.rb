8fc23f357d5ea1288b568b21e8a0c441a1052782
Ken Lenhart <omnipaid@gmail.com>
2022-09-19 20:39:32 -0700

    Add model relationship tests for dish_spec.rb, ingredient_spec.rb, dish_ingredients_spec.rb

---
+++ db/migrate/20220920033105_create_dish_ingedients.rb
@@ -0,0 +1,10 @@
+class CreateDishIngedients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :dish_ingedients do |t|
+      t.references :dish, foreign_key: true
+      t.references :ingredient, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -7,5 +7,7 @@ RSpec.describe Dish, type: :model do
   end
   describe "relationships" do
     it {should belong_to :chef}
+    it { should have_many(:dish_ingedients) }
+    it { should have_many(:ingredients).through(:dish_ingedients) }
   end
 end

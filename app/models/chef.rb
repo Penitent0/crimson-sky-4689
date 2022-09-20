23a33b1c766fd046b83f34ae050006e8870c2c64
Ken Lenhart <omnipaid@gmail.com>
2022-09-19 22:22:45 -0700

    test: Add ingredient_list model method to chef_spec.rb

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -1,5 +1,10 @@
 class Chef < ApplicationRecord
   validates_presence_of :name
   has_many :dishes
+  has_many :dish_ingredients, through: :dishes
+  has_many :ingredients, through: :dish_ingredients
 
+  def ingredient_list
+    ingredients.distinct.sort
+  end
 end
--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Chef, type: :model do
   end
   describe "relationships" do
     it {should have_many :dishes}
+    it { should have_many(:dish_ingredients).through(:dishes) }
+    it { should have_many(:ingredients).through(:dish_ingredients) }
   end
 
   describe '#instance methods' do
@@ -15,11 +17,21 @@ RSpec.describe Chef, type: :model do
     @dish_1 = @chef_1.dishes.create!(name: "Raviolis", description: "Cheap pasta in ketchup sauce")
     @dish_2 = @chef_1.dishes.create!(name: "Lasagna", description: "Cardboard noodles with sawdust sprinkles")
 
-    @ingredient_1 = @dish_1.ingredients.create!(name: "Ketchup Sauce", calories: 1000)
-    @ingredient_2 = @dish_1.ingredients.create!(name: "Cheap Pasta", calories: 2000)
+    @ingredient_1 = Ingredient.create!(name: "Ketchup Sauce", calories: 1000)
+    @ingredient_2 = Ingredient.create!(name: "Cheap Pasta", calories: 2000)
 
-    @ingredient_3 = @dish_2.ingredients.create!(name: "Cardboard Noodles", calories: 500)
-    @ingredient_4 = @dish_2.ingredients.create!(name: "Sawdust Sprinkles", calories: 1000)
+    @ingredient_3 = Ingredient.create!(name: "Cardboard Noodles", calories: 500)
+    @ingredient_4 = Ingredient.create!(name: "Sawdust Sprinkles", calories: 1000)
+
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_1)
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_2)
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_3)
+    DishIngredient.create!(dish: @dish_1, ingredient: @ingredient_4)
+
+    DishIngredient.create!(dish: @dish_2, ingredient: @ingredient_1)
+    DishIngredient.create!(dish: @dish_2, ingredient: @ingredient_2)
+    DishIngredient.create!(dish: @dish_2, ingredient: @ingredient_3)
+    DishIngredient.create!(dish: @dish_2, ingredient: @ingredient_4)
   end
     it 'ingredient_list' do
       expect(@chef_1.ingredient_list).to eq([@ingredient_1, @ingredient_2, @ingredient_3, @ingredient_4])

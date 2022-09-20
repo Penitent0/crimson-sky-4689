8648512430329a79e890ca842a84e23e01da332a
Ken Lenhart <omnipaid@gmail.com>
2022-09-19 21:26:35 -0700

    test: Add tests for User Story 2

--- spec/features/dish/show_spec.rb
+++ spec/features/dish/show_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Dish Show Page' do
+  before :each do
+    @chef_1 = Chef.create!(name: "Chef Boyardee")
+
+    @dish_1 = @chef_1.dishes.create!(name: "Raviolis", description: "Cheap pasta in ketchup sauce")
+    @dish_2 = @chef_1.dishes.create!(name: "Lasagna", description: "Cardboard noodles with sawdust sprinkles")
+
+    @ingredient_1 = @dish_1.ingredients.create!(name: "Ketchup Sauce", calories: 1000)
+    @ingredient_2 = @dish_1.ingredients.create!(name: "Cheap Pasta", calories: 2000)
+
+    @ingredient_3 = @dish_2.ingredients.create!(name: "Cardboard Noodles", calories: 500)
+    @ingredient_4 = @dish_2.ingredients.create!(name: "Sawdust Sprinkles", calories: 1000)
+  end
 
   # As a visitor
   # When I visit a dish's show page
@@ -9,19 +21,6 @@ RSpec.describe 'Dish Show Page' do
   # And I see the chef's name.
 
   describe 'User Story 1 - When I visit a dishs show page' do
-    before :each do
-      @chef_1 = Chef.create!(name: "Chef Boyardee")
-
-      @dish_1 = @chef_1.dishes.create!(name: "Raviolis", description: "Cheap pasta in ketchup sauce")
-      @dish_2 = @chef_1.dishes.create!(name: "Lasagna", description: "Cardboard noodles with sawdust sprinkles")
-
-      @ingredient_1 = @dish_1.ingredients.create!(name: "Ketchup Sauce", calories: 1000)
-      @ingredient_2 = @dish_1.ingredients.create!(name: "Cheap Pasta", calories: 2000)
-
-      @ingredient_3 = @dish_2.ingredients.create!(name: "Cardboard Noodles", calories: 500)
-      @ingredient_4 = @dish_2.ingredients.create!(name: "Sawdust Sprinkles", calories: 1000)
-    end
-
     it 'I see the dish’s name and description' do
       visit dish_path(@dish_1)
       
@@ -73,6 +72,20 @@ RSpec.describe 'Dish Show Page' do
       
       within "#dish-info" do
         expect(page).to have_content(@chef_1.name)
+      end
+    end
+  end
+
+  # As a visitor
+  # When I visit a dish's show page
+  # I see the total calorie count for that dish.
+
+  describe 'User Story 2 - When I visit a dishs show page' do
+    it 'I see the total calorie count for that dish.' do
+      visit dish_path(@dish_1)
+
+      within "#dish-total-calories" do
+        expect(page).to have_content(@dish_1.total_calories)
       end
     end
   end
